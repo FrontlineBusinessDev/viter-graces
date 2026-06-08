8674f5feeef4680532a450fa419173527c7d9596
Louren Rubico <louren.rubico@frontlinebusiness.com.ph>
2026-06-08 15:55:01 +0800

    cypress login is edited

--- cypress.config.js
+++ cypress.config.js
@@ -1,30 +1,40 @@
-require("dotenv").config();
+// require("dotenv").config();
 const { defineConfig } = require("cypress");
 
-// module.exports = defineConfig({
-//   e2e: {
-//     baseUrl: 'http://localhost:5173',
-//     viewportWidth: 1280,
-//     viewportHeight: 720,
-//     video: false,
-//     screenshotOnRunFailure: true,
-//   }
-// })
-
 module.exports = defineConfig({
-  env: {
-    email: process.env.CYPRESS_EMAIL,
-    password: process.env.CYPRESS_PASSWORD,
-    apiUrl: process.env.CYPRESS_API_URL,
+  e2e: {
+    baseUrl: "http://localhost:5173",
     viewportWidth: 1280,
     viewportHeight: 720,
     video: false,
     screenshotOnRunFailure: true,
   },
-
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
 });
+
+// module.exports = defineConfig({
+//   // env: {
+//   //   email: process.env.CYPRESS_EMAIL,
+//   //   password: process.env.CYPRESS_PASSWORD,
+//   //   apiUrl: process.env.CYPRESS_API_URL,
+//   //   viewportWidth: 1280,
+//   //   viewportHeight: 720,
+//   //   video: false,
+//   //   screenshotOnRunFailure: true,
+//   // },
+
+//   env: {
+//     email: "louren.rubico@frontlinebusiness.com.ph",
+//     pasword: "Louren23!",
+//     baseUrl: "http://localhost:5173",
+//     viewportWidth: 1280,
+//     viewportHeight: 720,
+//     video: false,
+//     screenshotOnRunFailure: true,
+//   },
+
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//   },
+// });
