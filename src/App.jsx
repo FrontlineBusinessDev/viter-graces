7b0700cf7ceec84d1fb0ab3ff3a4dfebbfb0a717
CyreneLumabas <cyrene.lumabas@frontlinebusiness.com.ph>
2026-06-15 10:11:32 +0800

    update new branch

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       ...routesDeveloper,
     ],
     {
-      basename: "/portal",
+      basename: "/",
     },
   );
 
--- src/layout/headers/TitleHeader.jsx
+++ src/layout/headers/TitleHeader.jsx
@@ -9,7 +9,8 @@ const TitleHeader = ({}) => {
   const userRole = store.credentials?.data?.role;
 
   const location = useLocation();
-  const currentTab = location.pathname.split("/")[3];
+  const currentTab = location.pathname.split("/")[2];
+  console.log("currentTab", currentTab);
 
   const currentHeader = titleHeaderTab().find((item) => {
     const hasTabs = item.array_tab.length > 0;
@@ -27,6 +28,7 @@ const TitleHeader = ({}) => {
     (tab) => tab.title_tab === currentTab,
   );
 
+  console.log("titleHeaderTab", titleHeaderTab());
   console.log("currentHeader", currentHeader);
 
   const formatText = (text) => text.replace(/-/g, " ");
