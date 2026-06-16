0eb1b81045be4ab6862f08deba92ae3da8c9bd98
Louren Rubico <louren.rubico@frontlinebusiness.com.ph>
2026-06-16 09:12:06 +0800

    cypress: commands.js is updated

--- cypress/e2e/products.cy.js
+++ cypress/e2e/products.cy.js
@@ -1,15 +1,7 @@
 describe("Products Module - CRUD Flow", () => {
   beforeEach(() => {
     cy.session("admin", () => {
-      cy.visit("/login");
-
-      cy.get("input[name=user_account_email]").type(Cypress.env("email"));
-
-      cy.get("input[name=password]").type(Cypress.env("password"));
-
-      cy.get("button[type=submit]").click();
-
-      cy.url().should("not.include", "/login");
+      cy.login();
     });
 
     cy.visit("/developer/products");
