958a161800286a882af6d24e6cd725659f308e42
Cyrhina <cyrenemlumabas@gmail.com>
2026-06-27 11:10:37 +0800

    update

--- src/custom-hooks/useDarkMode.jsx
+++ src/custom-hooks/useDarkMode.jsx
@@ -2,20 +2,16 @@ import { useEffect, useState } from "react";
 
 const useDarkMode = () => {
   const [darkMode, setDarkMode] = useState(() => {
-    return localStorage.getItem("theme") === "light";
+    return localStorage.getItem("theme") === "dark";
   });
 
   useEffect(() => {
     document.documentElement.classList.toggle("dark", darkMode);
-  }, [localStorage.getItem("theme"), darkMode]);
+    localStorage.setItem("theme", darkMode ? "dark" : "light");
+  }, [darkMode]);
 
   const toggleDarkMode = () => {
-    setDarkMode((prev) => {
-      const next = !prev;
-      localStorage.setItem("theme", next ? "dark" : "light");
-
-      return next;
-    });
+    setDarkMode((prev) => !prev);
   };
 
   return { darkMode, toggleDarkMode };
